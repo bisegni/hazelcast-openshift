96ff50d77adce1e842d3868d9e53e5b1f26001af
noctarius <me@noctarius.com>
2015-12-04 14:10:22 +0100

    basic openshift support

--- src/main/java/DiscoveryTest.java
+++ src/main/java/DiscoveryTest.java
@@ -1,12 +1,16 @@
+import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 
 public class DiscoveryTest {
 
     public static void main(String[] args) {
-        //System.setProperty("kubernetes.master", "https://10.245.1.2");
-
-        HazelcastInstance hazelcastInstance = Hazelcast.newHazelcastInstance();
+        String type = System.getenv("HAZELCAST_TYPE");
+        if ("client".equals(type)) {
+            HazelcastClient.newHazelcastClient();
+        } else {
+            Hazelcast.newHazelcastInstance();
+        }
     }
 
 }
